94629c64b234cc1b2082c01bcc48128cd235c4a5
gabriel-dp <gabriel.meira.2004@gmail.com>
2023-03-10 14:38:54 -0300

    :gear: Store points sorted by Y coordinate

--- src/point.c
+++ src/point.c
@@ -13,6 +13,22 @@ void inputError(char* message) {
     exit(EXIT_FAILURE);
 }
 
+void addPointSortedY(Point newPoint, Point** pointsArray, int total) {
+    int index = 0;
+
+    // Find array index to put the new point
+    while (index < total && newPoint.y >= (*pointsArray)[index].y) {
+        index++;
+    }
+
+    // Dislocates all elements after "index" to free that position
+    for (int i = total; i >= index; i--) {
+        (*pointsArray)[i + 1] = (*pointsArray)[i];
+    }
+
+    (*pointsArray)[index] = newPoint;
+}
+
 void getPointsFromInput(char* path, Point** points, int* totalPoints, int* xa, int* xb) {
     FILE* file = fopen(path, "r");
     if (file == NULL) inputError("Cannot read file");
@@ -22,8 +38,10 @@ void getPointsFromInput(char* path, Point** points, int* totalPoints, int* xa, i
     *points = createPointsArray(*totalPoints);
 
     for (int i = 0; i < *totalPoints; i++) {
-        if (fscanf(file, "%d %d", &((*points)[i].x), &((*points)[i].y)) != 2) {
+        Point newPoint;
+        if (fscanf(file, "%d %d", &newPoint.x, &newPoint.y) != 2) {
             inputError("Missing points");
         }
+        addPointSortedY(newPoint, points, i);
     }
 }
