880962b7d1fb98c9cd9297c6826c1201c20e7442
Davi dos Reis <davireisjesus@gmail.com>
2023-03-13 12:39:20 -0300

    :wrench: Change coordinate params to points

--- include/sequence.h
+++ include/sequence.h
@@ -14,17 +14,17 @@ typedef struct {
 
 // Functions used in main.c
 void printSequence(Sequence);
-Sequence getLongestPath(Sequence, Coordinate, Coordinate);
+Sequence getLongestPath(Sequence, Point, Point);
 
 // Functions used in pointio.c
 Sequence createSequence(int length);
 
 // Functions used only inside sequence.c
-int validPoint(Point, Point, Coordinate, Coordinate);
+int validPoint(Point, Point, Point, Point);
 void addPointToSequence(Sequence*, Point);
 void restoreSequence(Sequence*, int);
 void copySequence(Sequence, Sequence*);
 void sortSequenceByY(Sequence);
-void searchSequences(int, Point[], Sequence*, Sequence*, Coordinate, Coordinate);
+void searchSequences(int, Point[], Sequence*, Sequence*, Point, Point);
 
 #endif
