38fc9145faf5bc60feadd9eb961afc32428c0014
Davi Reis <davireisjesus@gmail.com>
2023-03-14 21:14:14 -0300

    :sparkles: Getting user instructions time

--- src/main.c
+++ src/main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+#include <sys/resource.h>
 
 #include "../include/args.h"
 #include "../include/pointio.h"
 #include "../include/sequence.h"
 
+
 int main(int argc, char* argv[]) {
+
+    struct rusage usage;
+    struct timeval start, end;
+    int who = RUSAGE_SELF;
+
+    int ret  = getrusage(who, &usage);
+    printf("\n\n|| %d ||\n\n", ret);
+    
+    start = usage.ru_utime;
+
     char *inputPath, *outputPath;
     getArgs(&inputPath, &outputPath, argc, argv);
 
@@ -15,6 +27,13 @@ int main(int argc, char* argv[]) {
     printSequence(longestPath);
 
     saveLongestLengthOutput(outputPath, longestPath.length);
+
+    getrusage(who, &usage);
+    end = usage.ru_utime;
+
+    printf("\nStarted at: %lu.%lus\n", start.tv_sec, start.tv_usec);
+    printf("Ended at: %lu.%lus\n", end.tv_sec, end.tv_usec);
+
 
     return 0;
 }
